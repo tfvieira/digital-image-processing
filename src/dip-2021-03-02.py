0b9fa5c9521040f05de28d3d786d4197e5c5183e
Tiago Vieira <tvieira@ic.ufal.br>
2021-03-02 15:26:35 -0300

    Added 2021-03-02 code.

--- src/dip-2021-03-02.py
+++ src/dip-2021-03-02.py
@@ -43,66 +43,40 @@ cv2.randu(bgr[0], 0, 255)
 cv2.randu(bgr[1], 0, 255)
 cv2.randu(bgr[2], 0, 255)
 img = cv2.merge(bgr)
-plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(16,4))
+plt.subplot("121"); plt.title("Original"); plt.imshow(img)
+plt.subplot("122"); plt.title("Histogram"); plot_rgb_histogram(img)
+plt.show()
 
 #%% Initializing a grayscale image with random values, normally distributed
 img = np.ones((250, 250), dtype=np.uint8)
 cv2.randn(img, 127, 40)
-cv2.imshow("img", img)
-cv2.waitKey(0)
+
+plt.figure(figsize=(16,4))
+plt.subplot("221"); plt.title("Original"); plt.imshow(img, cmap='gray')
+plt.subplot("222"); plt.title("Histogram"); plt.hist(img.ravel(), 256, [0, 256])
+plt.show()
 
 #%% Initializing a color image with random values, normally distributed
-img = np.ones((250, 250, 3), dtype=np.uint8)
-bgr = cv2.split(img)
-cv2.randn(bgr[0], 127, 40)
-cv2.randn(bgr[1], 127, 40)
-cv2.randn(bgr[2], 127, 40)
-img = cv2.merge(bgr)
-cv2.imshow("img", img)
-cv2.waitKey(0)
-
-#%% Initialize a color grayscale with uniformly distributed random values and visualize its histogram
-img = np.ones((250, 250), dtype=np.uint8)
-cv2.randu(img, 0, 255)
-plt.title("Histogram"); plt.hist(img.ravel(), 256, [0, 256])
-plt.show()
-
-#%% Initialize a color image with uniformly distributed random values and visualize its histogram
-img = np.ones((250, 250, 3), dtype=np.uint8)
-bgr = cv2.split(img)
-cv2.randu(bgr[0], 0, 255)
-cv2.randu(bgr[1], 0, 255)
-cv2.randu(bgr[2], 0, 255)
-img = cv2.merge(bgr)
-cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = np.ones((50, 50, 3), dtype=np.uint8)
+cv2.namedWindow("img", cv2.WINDOW_KEEPRATIO)
+while 0xFF & cv2.waitKey(1) != ord('q'):
+    bgr = cv2.split(img)
+    cv2.randn(bgr[0], 127, 40)
+    cv2.randn(bgr[1], 127, 40)
+    cv2.randn(bgr[2], 127, 40)
+    img = cv2.merge(bgr)
+    cv2.imshow("img", img)
+cv2.destroyAllWindows()
 
 plt.figure(figsize=(16,4))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 plt.subplot("121"); plt.title("Original"); plt.imshow(img)
 plt.subplot("122"); plt.title("Histogram"); plot_rgb_histogram(img)
 plt.show()
 
-
-#%% Initialize a grayscale image with normally distributed random values and visualize its histogram
-img = np.ones((250, 250), dtype=np.uint8)
-cv2.randn(img, 127, 40)
-plt.figure(figsize=(16,4))
-plt.subplot("121"); plt.title("Original"); plt.imshow(img,'gray')
-plt.subplot("122"); plt.title("Histogram"); plot_grayscale_histogram(img)
-plt.show()
-
-#%% Initialize a color image with normally distributed random values and visualize its histogram
-img = np.ones((250, 250, 3), dtype=np.uint8)
-bgr = cv2.split(img)
-cv2.randn(bgr[0], 127, 40)
-cv2.randn(bgr[1], 127, 40)
-cv2.randn(bgr[2], 127, 40)
-img = cv2.merge(bgr)
-cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-plt.figure(figsize=(16,4))
-plt.subplot("121"); plt.title("Original"); plt.imshow(img)
-plt.subplot("122"); plt.title("Histogram"); plot_rgb_histogram(img)
-plt.show()
 
 #%% Scale image
 img = np.ones((3, 3), dtype=np.float32)
@@ -119,11 +93,3 @@ print("Converted to uint8 = \n", img, "\n\n")
 img = 255 * img
 img = np.asarray(img, dtype=np.uint8)
 print(img, "\n\n")
-
-#%% Create random images continuously
-img = np.ones((250, 250), dtype=np.uint8)
-cv2.namedWindow("img", cv2.WINDOW_KEEPRATIO)
-while 0xFF & cv2.waitKey(1) != ord('q'):
-    cv2.randn(img, 120, 60)
-    cv2.imshow("img", img)
-cv2.destroyAllWindows()
